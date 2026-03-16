4871a5e56fd17a7a211dc411074556197c62962d
Danya <danyaputilin@yandex.ru>
2026-03-16 21:24:00 +0300

    Update const.py

--- pycode/const.py
+++ pycode/const.py
@@ -4,9 +4,6 @@
 
 
 # Пути к исполняемым файлам и программам на C++
-# COUNT_WORDS_PATH = "./src/count_words.cpp"
-# COUNT_WORDS_EXE_PATH = "./exe/count_words"
-PRINT_VOLAND_PATH = "./src/print_voland.cpp"
 PRINT_VOLAND_EXE_PATH = "./exe/main print_voland"
 MAIN_CPP_SCRIPT_PATH = "./src/*.cpp"
 MAIN_CPP_SCRIPT_EXE_PATH = "./exe/main"
